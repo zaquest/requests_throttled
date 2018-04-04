b69f219524e84d251571f6958b0e7ab04bc53241
zaquest <web.elektro.net@gmail.com>
2018-04-05 03:39:51 +0700

    Fix py_modules.

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='requests_throttled',
-      version='0.1',
+      version='0.2',
       description='Throttling requests.Session.',
       url='http://github.com/zaquest/requests_throttled',
       author='zaquest',
       author_email='web.elektro.net@gmail.com',
       license='MIT',
-      py_modules=['requests_trottled'],
+      install_requires=['requests'],
+      py_modules=['requests_throttled'],
       zip_safe=True)
